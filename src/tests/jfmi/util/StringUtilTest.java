6e3df29eec2b6bcbcb94e324848ef09ee135a281
Joram <jw.benham@gmail.com>
2012-10-08 14:43:47 -0300

    Implemented null-parameter tests for StringUtil

--- src/tests/jfmi/util/StringUtilTest.java
+++ src/tests/jfmi/util/StringUtilTest.java
@@ -2,15 +2,16 @@ package tests.jfmi.util;
 
 import org.junit.Test;
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
 
 import jfmi.util.StringUtil;
 
 public class StringUtilTest {
 
 	@Test
-	public void parseNameFromPathTest()
+	public void testParseNameFromPath_NonNullParams()
 	{
-		System.out.println("parseNameFromPathTest()");
+		System.out.println("testParseNameFromPath_NonNullParams()");
 
 		final String PATH = "some/path/filename";
 		final String NOPATH = "filename";
@@ -23,10 +24,17 @@ public class StringUtilTest {
 		assertTrue(result.equals(NOPATH));
 	}
 
-	@Test 
-	public void doubleQuoteTest()
+	@Test(expected= NullPointerException.class)
+	public void testParseNameFromPath_NullParams()
 	{
-		System.out.println("doubleQuoteTest()");
+		System.out.println("testParseNameFromPath_NullParams()");
+		StringUtil.parseNameFromPath(null);
+	}
+
+	@Test 
+	public void testDoubleQuote_NonNullParams()
+	{
+		System.out.println("testDoubleQuote_NonNullParams()");
 
 		final String UNQUOTED = "doubleQuote test string";
 		final String QUOTED = "\"doubleQuote test string\"";
@@ -34,6 +42,17 @@ public class StringUtilTest {
 
 		result = StringUtil.doubleQuote(UNQUOTED);
 		assertTrue(result.equals(QUOTED));
+	}
+
+	@Test 
+	public void testDoubleQuote_NullParams()
+	{
+		System.out.println("testDoubleQuote_NullParams()");
+		
+		final String expected = "\"null\"";
+		String actual = StringUtil.doubleQuote(null);
+
+		assertEquals(expected, actual);
 	}
 
 }
