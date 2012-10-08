fa009e92af8573ad86a3f172ba483a5351c72b45
Joram <jw.benham@gmail.com>
2012-10-08 13:49:49 -0300

    Wrote unit test for StringUtil.parseNameFromPath()

--- src/tests/jfmi/util/StringUtilTest.java
+++ src/tests/jfmi/util/StringUtilTest.java
@@ -7,8 +7,27 @@ import jfmi.util.StringUtil;
 
 public class StringUtilTest {
 
-	@Test public void doubleQuoteTest()
+	@Test
+	public void parseNameFromPathTest()
 	{
+		System.out.println("parseNameFromPathTest()");
+
+		final String PATH = "some/path/filename";
+		final String NOPATH = "filename";
+		String result;
+
+		result = StringUtil.parseNameFromPath(PATH);
+		assertTrue(result.equals(NOPATH));
+
+		result = StringUtil.parseNameFromPath(NOPATH);
+		assertTrue(result.equals(NOPATH));
+	}
+
+	@Test 
+	public void doubleQuoteTest()
+	{
+		System.out.println("doubleQuoteTest()");
+
 		final String UNQUOTED = "doubleQuote test string";
 		final String QUOTED = "\"doubleQuote test string\"";
 		String result;
