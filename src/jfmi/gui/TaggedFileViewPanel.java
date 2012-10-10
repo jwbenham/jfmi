d2f282dc5bb6ca3355a77e825636a116f641fa09
Joram <jw.benham@gmail.com>
2012-10-10 13:39:27 -0300

    Added skeleton class TaggedFileViewPanel

---
+++ src/jfmi/gui/TaggedFileViewPanel.java
@@ -0,0 +1,23 @@
+package jfmi.gui;
+
+
+
+/** A TaggedFileViewPanel displays all information contained in a TaggedFile
+  to the user, and allows the user to communicate changes to a
+  TaggedFileHandler.
+  */
+public class TaggedFileViewPanel {
+
+	// PRIVATE INSTANCE Fields
+
+
+	//************************************************************
+	// PUBLIC INSTANCE Methods
+	//************************************************************
+
+
+	//************************************************************
+	// PRIVATE INSTANCE Methods
+	//************************************************************
+
+}
