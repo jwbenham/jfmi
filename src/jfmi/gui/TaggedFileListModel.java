1233c61c2778c9337ca21ad86835acd45cf097be
Joram <jw.benham@gmail.com>
2012-10-24 16:57:39 -0300

    New class jfmi.gui.TaggedFileListModel

---
+++ src/jfmi/gui/TaggedFileListModel.java
@@ -0,0 +1,11 @@
+package jfmi.gui;
+
+import javax.swing.AbstractListModel;
+
+import jfmi.app.TaggedFile;
+
+/** A TaggedFileListModel acts as a data model for a Swing JList.
+  */
+public class TaggedFileListModel extends AbstractListModel<TaggedFile> {
+
+}
