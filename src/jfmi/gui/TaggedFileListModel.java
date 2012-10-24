77f5ff90a36641ec305e8f5449cb4122dabbb833
Joram <jw.benham@gmail.com>
2012-10-24 17:31:13 -0300

    Implemented TaggedFileJList ctors

--- src/jfmi/gui/TaggedFileListModel.java
+++ src/jfmi/gui/TaggedFileListModel.java
@@ -1,30 +1,47 @@
 package jfmi.gui;
 
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.ArrayList;
 import javax.swing.AbstractListModel;
 
 import jfmi.app.TaggedFile;
 
-/** A TaggedFileListModel acts as a data model for a Swing JList.
+/** A TaggedFileListModel acts as a data model for a Swing JList. Unlike the
+  DefaultListModel, a TaggedFileListModel allows a client to add/remove list
+  elements, and sort the list of elements.
   */
 public class TaggedFileListModel extends AbstractListModel<TaggedFile> {
 
 	// Private Instance Fields
-	private ArrayList<TaggedFile> listData;
+	private List<TaggedFile> listData;
 
 
 	//************************************************************
 	// PUBLIC Instance Methods
 	//************************************************************
 
-	/** Constructs a default instance.
+	/** Constructs a default instance using an ArrayList<TaggedFile> as the
+	  underlying Collection object.
 	  */
 	public TaggedFileListModel()
 	{
-		listData = new ArrayList<TaggedFile>();
+		this(new ArrayList<TaggedFile>());
+	}
+
+	/** Constructs a new instance using the specified non-null List<TaggedFile>.
+	  @param list the List to use as this instance's data
+	  @throws NullPointerException if list is null
+	  */
+	public TaggedFileListModel(List<TaggedFile> list)
+	{
+		if (list == null) {
+			throw new NullPointerException("list cannot be null");
+		}
+
+		listData = list;
 	}
 	
-
 	/** Returns a reference to the value at the specified index.
 	  @param index the index of the value to access
 	  @return a reference to the indexed value
@@ -46,7 +63,6 @@ public class TaggedFileListModel extends AbstractListModel<TaggedFile> {
 	//************************************************************
 	// PRIVATE Instance Methods
 	//************************************************************
-
 
 
 }
