e816d34913ec97d3f6e9e966890a4aa521c8bfea
Joram <jw.benham@gmail.com>
2012-10-24 21:58:36 -0300

    Added TaggedFileSorters.FilePathSorter

--- src/jfmi/app/TaggedFileSorters.java
+++ src/jfmi/app/TaggedFileSorters.java
@@ -1,5 +1,6 @@
 package jfmi.app;
 
+import java.io.File;
 import java.io.Serializable;
 import java.util.Comparator;
 
@@ -25,10 +26,49 @@ public class TaggedFileSorters {
 		  @return a negative integer, zero, or a positive integer, as the first
 				argument is less than, equal to, or greater than the second
 		  */
-		public int compare(TaggedFile o1, TaggedFile o2) {
+		public int compare(TaggedFile o1, TaggedFile o2) 
+		{
 			return o1.getFileId() - o2.getFileId();
 		}
 	}
+
+
+	/** Compares TaggedFile objects based on their file paths. */
+	public static class FilePathSorter implements
+		Comparator<TaggedFile>,
+		Serializable {
+
+		public static final long serialVersionUID = 24102012;	//dd/mm/yyyy
+
+		/** Compares the specified TaggedFile instances for order, using their 
+		  file paths.
+		  @param o1 a first object to be compared
+		  @param o2 a second object to be compared
+		  @return a negative integer, zero, or a positive integer, as the first
+				argument is less than, equal to, or greater than the second
+		  */
+		public int compare(TaggedFile o1, TaggedFile o2) 
+		{
+			File file1 = o1.getFile();
+			File file2 = o2.getFile();		
+
+			if (file1 == file2) {
+				return 0;
+			}
+
+			if (file1 == null) {
+				return -1;
+			}
+
+			if (file2 == null) {
+				return 1;
+			}
+
+			return file1.getPath().compareTo(file2.getPath());
+		}
+
+	}
+
 
 	/* Private to keep class from being instantiated. */
 	private TaggedFileSorters() {
